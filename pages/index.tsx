e8623a841cdd3fa1f76f3ac5ae10b7cf90d05764
Demilicz <Necrolick92@gmail.com>
2022-02-19 19:51:32 +0100

    next try to fix bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,16 +54,6 @@ const Home: NextPage<{products:ProductObject[]}> = ({ products, total } : InferG
 
           <div className={Style.card_container_flex}>
 
-            { error && <div className='error'>Something went wrong..</div> }
-
-            { loading &&  <div className='loading'>Its loading...</div> }
-
-            {
-             console.log(priceRange[0] === 0 && priceRange[1] === 100000, !products, products)
-            }
-
-
-
             { priceRange[0] === 0 && priceRange[1] === 100000 && arrayOfBrands.length === 3 && currentPage === 1 && products?.map((product:ProductObject) => {
               return  <Link href={`/${product.slug}&${product.sys.id}`} key={product.sys.id +"link"}>
                         <a className={Style.card_product}>
@@ -71,6 +61,10 @@ const Home: NextPage<{products:ProductObject[]}> = ({ products, total } : InferG
                         </a>
                       </Link>
             })}
+
+            { error && <div className='error'>Something went wrong..</div> }
+
+            { loading &&  <div className='loading'>Its loading...</div> }
 
             { priceRange[0] === 0 && priceRange[1] === 100000 &&  arrayOfBrands.length === 3 && currentPage > 1 && newData && newData.map((product:ProductObject) => {
               return  <Link href={`/${product.slug}&${product.sys.id}`} key={product.sys.id +"link"}>
